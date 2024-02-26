e9bd7752797cb7062be02469121530a3b7c75d6b
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-02-26 12:15:36 -0300

    Add files via upload

---
+++ prj_hello world/src/prj_hello world.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : prj_hello.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159
+
+int main(void) {
+
+		int idade = 17;
+		const int MAX_NUM =100;
+
+		printf("Minha variável int comum = %d\n\n", idade);
+		printf("Minha variável int constante = %d\n\n", MAX_NUM);
+		printf("Minha constante = %f\n", PI);
+
+		// MAX_NUM = 99;
+
+		return EXIT_SUCCESS;
+}

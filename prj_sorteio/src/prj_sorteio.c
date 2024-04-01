86cfd60aa62b65d5d9a53d9b51a439979bce1f6d
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-04-01 11:06:44 -0300

    Add files via upload

---
+++ prj_sorteio/src/prj_sorteio.c
@@ -0,0 +1,34 @@
+/*
+ ============================================================================
+ Name        : prj_sorteio.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+
+		//Inicializa a semente para a função rand() usando o tempo atual
+		srand(time(NULL));
+
+		//Gera um número aleatório entre 0 e 100
+		int numSorteado = rand () % 101;
+
+		printf("Loteria");
+		printf("\nEscolha um número entre 0 e 100:");
+		int numEscolhido;
+		scanf("%d", &numEscolhido);
+
+		if(numEscolhido == numSorteado) {
+			printf("\nVocê ganhou|");
+		}else{
+			printf("\nVocê perdeu! O número sorteado foi: %d", numSorteado);
+		}
+	return 0;
+}
